11c36c43aab17ce7d3d257dbd3947952fded482c
Tom Kiernan <tkiernan120@gmail.com>
2018-11-21 17:32:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,9 @@
    //
 
    const privateAPIs = {
-    titles: ["Mr"," Mrs"," Miss"," Ms"," Dr"," Admiral"," Air Comm"," Ambassador"," Baron"," Baroness"," Brig &amp; Mrs"," Brig Gen"," Brigadier"," Brother"," Canon"," Capt"," Chief"," Cllr"," Col"," Commander"," Commander &amp; Mrs"," Consul"," Consul General"," Count"," Countess"," Countess of"," Cpl"," Dame"," Deputy"," Dr &amp; Mrs"," Drs"," Duchess"," Duke"," Earl"," Father"," General"," Gräfin"," HE"," HMA"," Her Grace"," His Excellency"," Ing"," Judge"," Justice"," Lady"," Lic"," Llc"," Lord"," Lord &amp; Lady"," Lt"," Lt Col"," Lt Cpl"," M"," Madam"," Madame"," Major"," Major General"," Marchioness"," Marquis"," Minister"," Mme"," Mr &amp; Dr"," Mr &amp; Mrs"," Mr &amp; Ms"," Prince"," Princess"," Prof"," Prof &amp; Dr"," Prof &amp; Mrs"," Prof &amp; Rev"," Prof Dame"," Prof Dr"," Pvt"," Rabbi"," Rear Admiral"," Rev"," Rev &amp; Mrs"," Rev Canon"," Rev Dr"," Senator"," Sgt"," Sheriff"," Sir"," Sir &amp; Lady"," Sister"," The Earl of"," The Hon"," The Hon Dr"," The Hon Lady"," The Hon Lord"," The Hon Mrs"," The Hon Sir"," The Honourable"," The Rt Hon"," The Rt Hon Dr"," The Rt Hon Lord"," The Rt Hon Sir"," The Rt Hon Visc"," Viscount"],
+    titles: ["Mr"," Mrs"," Miss"," Ms"," Dr"," Admiral"," Air Comm"," Ambassador"," Baron"," Baroness"," Brig &amp; Mrs"," Brig Gen"," Brigadier"," Brother"," Canon"," Capt"," Chief"," Cllr"," Col"," Commander"," Commander &amp; Mrs"," Consul"," Consul General"," Count"," Countess"," Countess of"," Cpl"," Dame"," Deputy"," Dr &amp; Mrs"," Drs"," Duchess"," Duke"," Earl"," Father"," General"," Gräfin"," HE"," HMA"," Her Grace"," His Excellency"," Ing"," Judge"," Justice"," Lady"," Lic"," Llc"," Lord"," Lord &amp; Lady"," Lt"," Lt Col"," Lt Cpl"," M"," Madam"," Madame"," Major"," Major General"," Marchioness"," Marquis"," Minister"," Mme"," Mr &amp; Dr"," Mr &amp; Mrs"," Mr &amp; Ms"," Prince"," Princess"," Prof"," Prof &amp; Dr"," Prof &amp; Mrs"," Prof &amp; Rev"," Prof Dame"," Prof Dr"," Pvt"," Rabbi"," Rear Admiral"," Rev"," Rev &amp; Mrs"," Rev Canon"," Rev Dr"," Senator"," Sgt"," Sheriff"," Sir"," Sir &amp; Lady"," Sister"," The Earl of"," The Hon"," The Hon Dr"," The Hon Lady"," The Hon Lord"," The Hon Mrs"," The Hon Sir"," The Honourable"," The Rt Hon"," The Rt Hon Dr"," The Rt Hon Lord"," The Rt Hon Sir"," The Rt Hon Visc"," Viscount"].map( (e) => {
+      return e.toLowerCase();
+    }),
     punctuation: [],
 
    }
@@ -68,12 +70,11 @@
     if( publicAPIs.parts.length ){
 
       publicAPIs.parts.map((e,i) => {
-        let itemIndex = privateAPIs.titles.indexOf( e.replace( /[^\d]/g, "" ).toLowerCase() );
-        if( itemIndex ){
+        let itemIndex = privateAPIs.titles.indexOf( e.replace( /[^\w\d]/g, "" ).toLowerCase() );
+        if( itemIndex < -1 ){
           publicAPIs.parsedData.title = privateAPIs.titles[itemIndex];
         }
       });
-
     }
    }
 
